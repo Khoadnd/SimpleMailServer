1385aa16d547097489a438b4fa9d67bd79687892
Khoa Đoàn Nguyễn Đăng <20521463@ms.uit.edu.vn>
2022-05-23 10:08:53 +0700

    Modified driver

---
+++ MailServer/Driver.cs
@@ -0,0 +1,22 @@
+﻿using MailServer.Core;
+
+namespace MailServer
+{
+    public class Driver
+    {
+        public static void Main(string[] args)
+        {
+            ServerCore.InitializeComponent();
+
+            // initialize server
+            SSMTP.SSMTPServer ssmtpServer = new SSMTP.SSMTPServer();
+            ssmtpServer.Start();
+
+            SIMAP.SIMAPServer simapServer = new SIMAP.SIMAPServer();
+            simapServer.Start();
+
+            // initialize mail delivery system
+            MsgDelivery.CreateQueueWatcher();
+        }
+    }
+}
